466b5fbbab5152f8d170d69e5bd5bf5f24544b55
Andrzej Jabłoński <andrzej@audioszum.pl>
2019-06-12 12:28:51 +0200

    Adds text content to file thirdWeek.html

--- js/test.js
+++ js/test.js
@@ -1,5 +1,3 @@
-console.log(`Witaj na stronie!`);
-
 let age; - 'zmienna zadeklarowana'
 let name = 'Andrzej Jabłoński'; - 'zmienna z przypisaną zawartością (zainicjalizowana)'
 console.log(`Nazywam się ${name}`);
