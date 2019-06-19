4f6b93642b0fb36954ed0ac1e31cd6354c0ae030
Andrzej Jabłoński <andrzej@audioszum.pl>
2019-06-19 10:03:31 +0200

    fix: Changes class name feat: Adds text content to file fourthWeek.html

--- js/script.js
+++ js/script.js
@@ -3,3 +3,6 @@ console.log(`Witaj na stronie!`);
 let age; - 'zmienna zadeklarowana'
 let name = 'Andrzej Jabłoński'; - 'zmienna z przypisaną zawartością (zainicjalizowana)'
 console.log(`Nazywam się ${name}`);
+
+const hello = (name, age) => console.log(`Na strone serdecznie zaprasza cię ${name} administrator ${age} letni.`);
+hello('Andrzej', 30);
