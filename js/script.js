de1661b30d8b3b513773aaeeb35d4bcac9f03dbd
Andrzej Jabłoński <andrzej@audioszum.pl>
2019-06-25 00:49:51 +0200

    feat: rebuilt main menu

--- js/script.js
+++ js/script.js
@@ -1,25 +1,20 @@
 // main menu
 const button = document.querySelector('.menuButton');
 
-const pullOutMenu = () => {
+button.addEventListener('click', () => {
     const line = document.querySelector('.menuButton__line-js');
+    const mainMenu = document.querySelector('.main__menu');
     if (!button.classList.contains('menuButton__switch-off') & !button.classList.contains('menuButton__switch-on')) {
         button.classList.add('menuButton__switch-on');
         line.classList.add('switch__line-on');
+        mainMenu.classList.add('main__menu-on');
     } else {
         line.classList.toggle('switch__line-off');
         line.classList.toggle('switch__line-on');
         button.classList.toggle('menuButton__switch-on');
         button.classList.toggle('menuButton__switch-off');
+        mainMenu.classList.toggle('main__menu-on');
+        mainMenu.classList.toggle('main__menu-off');
     }
-}
-button.addEventListener('click', pullOutMenu);
+});
 // main menu
-console.log(`Witaj na stronie!`);
-
-let age; - 'zmienna zadeklarowana'
-let name = 'Andrzej Jabłoński'; - 'zmienna z przypisaną zawartością (zainicjalizowana)'
-console.log(`Nazywam się ${name}`);
-
-const hello = (name, age) => console.log(`Na strone serdecznie zaprasza cię ${name} administrator ${age} letni.`);
-hello('Andrzej', 30);
