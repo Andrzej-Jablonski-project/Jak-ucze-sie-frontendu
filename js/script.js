6f10ce41ca7d30a32fad12ae39af05455b9939bb
Andrzej Jabłoński <andrzej@audioszum.pl>
2019-06-27 16:27:14 +0200

    feat: refactoring the HTML structure

--- js/script.js
+++ js/script.js
@@ -3,18 +3,18 @@ const button = document.querySelector('.menuButton');
 
 button.addEventListener('click', () => {
     const line = document.querySelector('.menuButton__line-js');
-    const mainMenu = document.querySelector('.main__menu');
+    const mainMenu = document.querySelector('.menu');
     if (!button.classList.contains('menuButton__switch-off') & !button.classList.contains('menuButton__switch-on')) {
         button.classList.add('menuButton__switch-on');
         line.classList.add('switch__line-on');
-        mainMenu.classList.add('main__menu-on');
+        mainMenu.classList.add('menu-on');
     } else {
         line.classList.toggle('switch__line-off');
         line.classList.toggle('switch__line-on');
         button.classList.toggle('menuButton__switch-on');
         button.classList.toggle('menuButton__switch-off');
-        mainMenu.classList.toggle('main__menu-on');
-        mainMenu.classList.toggle('main__menu-off');
+        mainMenu.classList.toggle('menu-on');
+        mainMenu.classList.toggle('menu-off');
     }
 });
 // main menu
