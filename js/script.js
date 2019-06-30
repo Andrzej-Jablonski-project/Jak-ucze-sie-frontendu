75a6536cc0baba7cb51b5c88f92712eeb20c12a6
Andrzej Jabłoński <andrzej@audioszum.pl>
2019-06-30 17:38:03 +0200

    feat: adds the type hamburger menu for article

--- js/script.js
+++ js/script.js
@@ -1,20 +1,37 @@
 // main menu
-const button = document.querySelector('.menuButton');
+const menuButton = document.querySelector('.menuButton');
 
-button.addEventListener('click', () => {
+menuButton.addEventListener('click', (e) => {
     const line = document.querySelector('.menuButton__line-js');
     const mainMenu = document.querySelector('.menu');
-    if (!button.classList.contains('menuButton__switch-off') & !button.classList.contains('menuButton__switch-on')) {
-        button.classList.add('menuButton__switch-on');
+    if (!menuButton.classList.contains('menuButton__switch-off') & !menuButton.classList.contains('menuButton__switch-on')) {
+        menuButton.classList.add('menuButton__switch-on');
         line.classList.add('switch__line-on');
         mainMenu.classList.add('menu-on');
     } else {
         line.classList.toggle('switch__line-off');
         line.classList.toggle('switch__line-on');
-        button.classList.toggle('menuButton__switch-on');
-        button.classList.toggle('menuButton__switch-off');
+        menuButton.classList.toggle('menuButton__switch-on');
+        menuButton.classList.toggle('menuButton__switch-off');
         mainMenu.classList.toggle('menu-on');
         mainMenu.classList.toggle('menu-off');
+    }
+});
+const articleButton = document.querySelector('.article__button');
+
+articleButton.addEventListener('click', (e) => {
+    const articleButtonLine = document.querySelector('.article__button__line-js');
+    const articleMenu = document.querySelector('.article__list');
+    if (!articleButton.classList.contains('menuButton__switch-off') & !articleButton.classList.contains('menuButton__switch-on')) {
+        articleButton.classList.add('menuButton__switch-on');
+        articleButtonLine.classList.add('switch__line-on');
+        articleMenu.classList.add('article__list-on');
+    } else {
+        articleButtonLine.classList.toggle('switch__line-off');
+        articleButtonLine.classList.toggle('switch__line-on');
+        articleButton.classList.toggle('menuButton__switch-on');
+        articleButton.classList.toggle('menuButton__switch-off');
+        articleMenu.classList.toggle('article__list-on');
     }
 });
 // main menu
